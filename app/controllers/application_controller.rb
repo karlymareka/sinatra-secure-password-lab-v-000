70b77a25ed1876e983cd1cd2941eaedb4c428ff1
Karly Mareka <karly.mareka@gmail.com>
2019-07-22 22:36:51 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,6 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     user = User.new(:username => params[:username], :password => params[:password])
-    binding.pry 
 		if user.save && user.username.length != 0   
       redirect "/login"
     else
